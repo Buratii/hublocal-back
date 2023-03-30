0c90a18cacd6b929b6e43d6e9ab4ce448b2ff8f1
Leonardo Burati <leonardoburati@hotmail.com>
2023-03-30 15:14:17 -0300

    feat: implement helpers

---
+++ src/helpers/messages.helper.ts
@@ -0,0 +1,6 @@
+export const MessagesHelper = {
+  VALIDATE_PASSWORD:
+    'Password should have uppercase and lowercase, numbers and special characters',
+  INVALID_AUTH: 'E-mail and/or password invalid.',
+  INVALID_CNPJ: 'invalid CNPJ.',
+};
